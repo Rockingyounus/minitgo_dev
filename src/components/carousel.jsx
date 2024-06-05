70a2ba520dc9d0a64b6c575181f7cd8c822b06f2
Ganesh-Mern <raghabm7@gmail.com>
2024-06-05 14:51:09 +0530

    carousel img size changes

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -4,11 +4,15 @@ import { useContext } from "react";
 import myContext from "./context/MyContext";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart, showSnackbar, hideSnackbar } from "./redux/Slices/CartSlice";
+import {
+  addToCart,
+  showSnackbar,
+  hideSnackbar,
+} from "./redux/Slices/CartSlice";
 
 export default function Carousel() {
   const dispatch = useDispatch();
-  const context = useContext(myContext)
+  const context = useContext(myContext);
   const { products, snackbarOpen } = context;
 
   const handleAddToCart = (product, index) => {
@@ -18,28 +22,29 @@ export default function Carousel() {
     // Wait for 1 second, then hide snackbar
     setTimeout(() => {
       dispatch(hideSnackbar());
-    }, 1000)
+    }, 1000);
   };
-  const cart = useSelector(state => state.cart);
-
-
-
+  const cart = useSelector((state) => state.cart);
 
   return (
     <div>
       <div className="container ">
         <div className="text-left">
           <h3 className=" ">Trending Section</h3>
-          <span className=" text-muted p-2" style={{ fontSize: 13.5 }}>Nearst trending products</span>
+          <span className=" text-muted p-2" style={{ fontSize: 13.5 }}>
+            Nearst trending products
+          </span>
           <div className="rounded-full"></div>
         </div>
-        
+
         <div className="d-flex gap-3 overflow-x-scroll my-3  px-4 scroll-smooth ">
-        
           {products.map((prod, index) => (
-            // code started by Ganesh 
-            //paddding 4px 
-            <div key={index} className="prod p-4 shadow rounded bg-light my-4   ">
+            // code started by Ganesh
+            //paddding 4px
+            <div
+              key={index}
+              className="prod p-4 shadow rounded bg-light my-4   "
+            >
               {/* end code by Ganesh  */}
               <a
                 href={`/${prod.product_id}`}
@@ -51,9 +56,21 @@ export default function Carousel() {
                 className=" text-black"
               >
                 {/* code started by ganesh */}
-                <div key={index} className="d-flex flex-column mb-2 " style={{height:'500px'}} >
+                <div
+                  key={index}
+                  className="d-flex flex-column mb-2 "
+                  style={{ height: "500px" }}
+                >
+                  {/* {/* code end by ganesh * */}
+                  {/* code started by ganesh  */}
+                  <div style={{width:"100%" ,height:"55%"}}>
+                    <img
+                      className="w-100 rounded h-100"
+                      src={prod.product_image1}
+                      alt={`Image ${prod.product_id}`}
+                    />
+                  </div>
                   {/* code end by ganesh */}
-                  <img className="w-100 rounded" src={prod.product_image1} alt={`Image ${prod.product_id}`} />
                   <div className="d-flex flex-column justify-content-between p-2">
                     <div className="d-flex flex-column">
                       {/* /// code startd by Ganesh */}
@@ -63,7 +80,9 @@ export default function Carousel() {
                       {/* code started by ganesh */}
                       <p className="line-clamp-2">{prod.product_discription}</p>
                       {/* code end by ganesh */}
-                      <p className="font-bold" style={{ fontWeight: '700' }}>{prod.product_size}</p>
+                      <p className="font-bold" style={{ fontWeight: "700" }}>
+                        {prod.product_size}
+                      </p>
                     </div>
                     {cart.snackbar.open && cart.snackbar.index === index && (
                       <div
@@ -73,23 +92,32 @@ export default function Carousel() {
                         {cart.snackbar.message}
                       </div>
                     )}
-
                   </div>
                 </div>
               </a>
               <div className="d-flex justify-content-center align-items-center fs-4 mt-auto my-2 mb-3 ">
-
-                <button className="btn  btn-dark w-100 " style={{ whiteSpace: 'nowrap' }} onClick={() => handleAddToCart(prod, index)}> Add To Cart</button>
+                <button
+                  className="btn  btn-dark w-100 "
+                  style={{ whiteSpace: "nowrap" }}
+                  onClick={() => handleAddToCart(prod, index)}
+                >
+                  {" "}
+                  Add To Cart
+                </button>
                 <div className="d-flex ">
                   <button className="btn btn-dark w-100 mx-2 px-5 ">
-                    <Link to="/checkout" style={{ textDecoration: 'none', color: 'white' }}> Buy</Link>
+                    <Link
+                      to="/checkout"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      {" "}
+                      Buy
+                    </Link>
                   </button>
                 </div>
               </div>
             </div>
-
           ))}
-          
         </div>
       </div>
     </div>
